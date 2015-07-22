e27a02a7775490b79ef332b138e84d7020f1a87f
Suyeol Jeon <devxoul@gmail.com>
2015-07-22 21:09:58 +0900

    Initial Commit :D

---
+++ AsyncBlockOperation/AsyncBlockOperation.m
@@ -0,0 +1,68 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Suyeol Jeon (xoul.kr)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#import "AsyncBlockOperation.h"
+
+#pragma mark - AsyncBlockOperation
+
+@implementation AsyncBlockOperation
+
+- (nonnull instancetype)initWithBlock:(nonnull AsyncBlock)block {
+    self = [super init];
+    if (self) {
+        self.block = block;
+    }
+    return self;
+}
+
++ (nonnull instancetype)blockOperationWithBlock:(nonnull AsyncBlock)block {
+    AsyncBlockOperation *operation = [[AsyncBlockOperation alloc] initWithBlock:block];
+    return operation;
+}
+
+- (void)start {
+    self.isExecuting = YES;
+    if (self.block) {
+        self.block(self);
+    } else {
+        [self complete];
+    }
+}
+
+- (void)complete {
+    self.isExecuting = NO;
+    self.isFinished = YES;
+}
+
+@end
+
+
+#pragma mark - NSOperationQueue+AsyncBlockOperation
+
+@implementation NSOperationQueue (AsyncBlockOperation)
+
+- (void)addOperationWithAsyncBlock:(nonnull AsyncBlock)block {
+    AsyncBlockOperation *operation = [AsyncBlockOperation blockOperationWithBlock:block];
+    [self addOperation:operation];
+}
+
+@end
---
+++ README.md
@@ -0,0 +1,12 @@
+AsyncBlockOperation
+===================
+
+[![CocoaPods](http://img.shields.io/cocoapods/v/AsyncBlockOperation.svg?style=flat)](https://cocoapods.org/pods/AsyncBlockOperation)
+
+NSOperation subclass for support async block.
+
+
+License
+-------
+
+AsyncBlockOperation is under MIT license. See the LICENSE file for more info.
